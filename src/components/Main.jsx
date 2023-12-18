58fc7234460e7b3f79d7020044a0499c3161ecde
Lisa Pääjärvi <lisapaajarvi@hotmail.com>
2023-12-18 20:07:19 +0100

    categories

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,35 +1,86 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import SongItem from "./SongItem";
+import AddSongModal from "./AddSongModal";
+import { Button } from "flowbite-react";
 import { SongContext } from "../contexts/songContext";
 
 const Main = () => {
-    const { songs } = useContext(SongContext);
-    const handleClick = () => {
-        
+  const { songs } = useContext(SongContext);
+  const [openModal, setOpenModal] = useState(false);
+  const [editingContent, setEditingContent] = useState(null);
+  const songCount = songs.length;
+  const handleClick = (song) => {
+    setEditingContent(song);
+  };
+
+  useEffect(() => {
+    if (editingContent != null) {
+      setOpenModal(true);
     }
-    return(
-        <main className="h-[calc(100vh-6rem)] bg-emerald-100">
-            <div className="container mx-auto px-4">
-                <h2 className="text-lg flex justify-center">Sånger i repertoaren:</h2>
-                <div className="grid grid-cols-10">
-                    <p className="col-span-2">Titel</p>
-                    <p className="col-span-2">Kompositör</p>
-                    <p className="col-span-2">Noter</p>
-                    <p>Starttoner</p>
-                    <p>Längd</p>
-                    <p className="col-span-2">Kommentarer</p>
-                </div>
-                <section>
-                    {songs.map((song, index)=> {
-                        return <div key={index} className="bg-white even:bg-gray-200 flex my-2 p-2" onClick={(e)=> handleClick(e)}>
-                            <SongItem song={song}/>
-                        </div>
-                    })}
-                </section>
-                
-            </div>
-        </main>
-    )
-}
+  }, [editingContent]);
+
+  const onCloseModal = () => {
+    setOpenModal(false);
+    setEditingContent(null);
+  };
+
+  const categories = [
+    { name: "Jul", bgColor: "bg-red-400" },
+    { name: "Sakralt", bgColor: "bg-green-400" },
+    { name: "Film/tv/spel", bgColor: "bg-blue-400" },
+    { name: "Folkligt", bgColor: "bg-yellow-400" },
+    { name: "Klassiskt", bgColor: "bg-pink-400" },
+  ];
+
+  return (
+    <main className="min-h-[calc(100vh-6rem)] bg-emerald-100">
+      <div className="container mx-auto p-4">
+        <Button onClick={() => setOpenModal(true)}>Lägg till sång</Button>
+        <h2 className="text-lg flex justify-center">
+          Sånger i repertoaren ({songCount} st):
+        </h2>
+        <div className="flex gap-4 justify-around">
+          {categories.map((cat, index) => {
+            return (
+              <div
+                key={index}
+                className="rounded-md w-40 p-8 bg-red-400 flex align-center justify-center"
+              >
+                {cat.name}
+              </div>
+            );
+          })}
+        </div>
+        <div className="grid grid-cols-12">
+          <p className="col-span-2">Titel</p>
+          <p className="col-span-2">Kompositör</p>
+          <p className="col-span-2">Noter</p>
+          <p>Starttoner</p>
+          <p>Längd</p>
+          <p className="col-span-2">Kommentarer</p>
+          <p className="col-span-2">Kategori</p>
+        </div>
+        <section>
+          {songs.map((song, index) => {
+            return (
+              <div
+                key={index}
+                className="bg-white even:bg-gray-200 flex my-2 p-2"
+                onClick={() => handleClick(song)}
+              >
+                <SongItem song={song} />
+              </div>
+            );
+          })}
+        </section>
+      </div>
+      <AddSongModal
+        openModal={openModal}
+        onCloseModal={onCloseModal}
+        song={editingContent}
+      />
+    </main>
+  );
+};
 
 export default Main;
--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -1,29 +1,31 @@
-import PropTypes from 'prop-types';
-const SongItem = ({song}) => {
-    return(
-        <>
-            <h3 className="w-1/5">{song.title}</h3>
-            <p className="w-1/5">{song.composer}</p>
-            <p className="w-1/5">{song.origin}</p>
-            <div className="w-1/5 flex">
-                <p className="flex-1">{song.start}</p>
-                <p className="flex-1">{song.length}</p>
-            </div>
-            
-            <p className="w-1/5">{song.comments}</p>
-        </>
-    )
-}
+import PropTypes from "prop-types";
+const SongItem = ({ song }) => {
+  return (
+    <>
+      <h3 className="w-1/6">{song.title}</h3>
+      <p className="w-1/6">{song.composer}</p>
+      <p className="w-1/6">{song.origin}</p>
+      <div className="w-1/6 flex">
+        <p className="flex-1">{song.start}</p>
+        <p className="flex-1">{song.length}</p>
+      </div>
+
+      <p className="w-1/6">{song.comments}</p>
+      <p className="w-1/6">{song.category}</p>
+    </>
+  );
+};
 
 SongItem.propTypes = {
-    song: PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      composer: PropTypes.string.isRequired,
-      origin: PropTypes.string.isRequired,
-      start: PropTypes.string.isRequired,
-      length: PropTypes.string.isRequired,
-      comments: PropTypes.string.isRequired,
-    }).isRequired,
-  };
+  song: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    composer: PropTypes.string.isRequired,
+    origin: PropTypes.string.isRequired,
+    start: PropTypes.string.isRequired,
+    length: PropTypes.string.isRequired,
+    comments: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
-export default SongItem
+export default SongItem;
