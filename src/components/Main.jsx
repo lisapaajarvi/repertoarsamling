342c9be892ae8bbdc7587337561ba1ba1e36cf45
Lisa Pääjärvi <lisapaajarvi@hotmail.com>
2023-12-18 22:29:09 +0100

    added fuse search

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,16 @@
 import { useContext, useState, useEffect } from "react";
 import SongItem from "./SongItem";
 import AddSongModal from "./AddSongModal";
-import { Button } from "flowbite-react";
+import { Button, TextInput } from "flowbite-react";
 import { SongContext } from "../contexts/songContext";
+import Fuse from "fuse.js";
+
+const fuseOptions = {
+  shouldSort: true,
+  threshold: 0.4,
+  minMatchCharLength: 2,
+  keys: ["title", "composer", "comments"],
+};
 
 const Main = () => {
   const { songs } = useContext(SongContext);
@@ -10,6 +18,7 @@ const Main = () => {
   const [editingContent, setEditingContent] = useState(null);
   const [categoryFilter, setCategoryFilter] = useState("Alla");
   const [filteredSongs, setFilteredSongs] = useState(songs);
+  const [fuseSearch, setFuseSearch] = useState("");
   const songCount = filteredSongs.length;
 
   const handleClick = (song) => {
@@ -48,6 +57,19 @@ const Main = () => {
   console.log(categoryFilter);
   console.log(filteredSongs);
 
+  const handleSearch = () => {
+    const fuse = new Fuse(songs, fuseOptions);
+    if (fuseSearch.length > 2) {
+      const result = fuse.search(fuseSearch);
+      const resultArray = [];
+      result.forEach((item) => {
+        resultArray.push(item.item);
+      });
+      setFilteredSongs(resultArray);
+      setFuseSearch("");
+    }
+  };
+
   return (
     <main className="min-h-[calc(100vh-6rem)] bg-emerald-100">
       <div className="container mx-auto p-4">
@@ -68,6 +90,17 @@ const Main = () => {
               </div>
             );
           })}
+        </div>
+        <div>
+          <TextInput
+            id="search"
+            placeholder="Skriv din sökning här"
+            value={fuseSearch}
+            onChange={(e) => setFuseSearch(e.target.value)}
+          />
+          <div className="w-full">
+            <Button onClick={handleSearch}>Sök</Button>
+          </div>
         </div>
         <div className="grid grid-cols-12">
           <p className="col-span-2">Titel</p>
