2b8c57b8b5ee72e70adc0a3dda2beb9ec902abcc
Lisa Pääjärvi <lisapaajarvi@hotmail.com>
2023-12-18 20:05:20 +0100

    songs from db

--- src/contexts/songContext.jsx
+++ src/contexts/songContext.jsx
@@ -1,26 +1,60 @@
+/* eslint-disable react/prop-types */
 import { createContext, useEffect, useState } from "react";
+import { db } from "../../firebase.config";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
 
 export const SongContext = createContext({
   songs: [],
   addSong: () => {
     console.warn("NO PROVIDER FOUND");
   },
-
+  editSong: () => {
+    console.warn("NO PROVIDER FOUND");
+  },
 });
 
-function SongProvider({children}) {
+function SongProvider({ children }) {
   const [songs, setSongs] = useState([]);
+  const dbCollection = collection(db, "songs");
+
+  const getSongs = async () => {
+    getDocs(dbCollection).then((data) => {
+      setSongs(
+        data.docs.map((item) => {
+          return { ...item.data(), id: item.id };
+        })
+      );
+    });
+  };
 
   useEffect(() => {
-    setSongs([{"title":"Jul jul", "composer": "H M", "origin": "Lösa noter på drive", "start": "A, C, E", "length": "1:25", "comments": "En klassiker som folk gillar!"}, {"title":"Jul jul", "composer": "H M", "origin": "Lösa noter på drive", "start": "A, C, E", "length": "1:25", "comments": "En klassiker som folk gillar!"}])
+    getSongs();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const addSong = (newSong) => {
-    setSongs((prevState) => [...prevState, newSong]);
+    addDoc(dbCollection, {
+      ...newSong,
+    }).then(() => {
+      setSongs((prevState) => [...prevState, newSong]);
+    });
+  };
+
+  const editSong = (songToEdit) => {
+    const dbDocument = doc(db, "songs", songToEdit.id);
+    updateDoc(dbDocument, songToEdit).then(() => {
+      getSongs();
+    });
   };
 
   return (
-    <SongContext.Provider value={{ songs, addSong }}>
+    <SongContext.Provider value={{ songs, addSong, editSong }}>
       {children}
     </SongContext.Provider>
   );
